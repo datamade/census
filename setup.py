235972f1d200d856e463923ae8f408b0265d13a5
Jeremy Carbaugh <jcarbaugh@gmail.com>
2013-03-04 10:30:31 -0500

    use latest versions of requests and us packages

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ setup(
             "Operating System :: OS Independent",
             "Programming Language :: Python",
       ],
-      install_requires=['requests', 'us'],
+      install_requires=['requests==1.1.0', 'us==0.7'],
 )
