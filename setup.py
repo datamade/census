e0e9d2e5e1c32c8403d0d7e41952b2a5b82c888b
Forest Gregg <fgregg@uchicago.edu>
2019-01-16 19:40:25 -0600

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.rst').read()
 
 setup(
     name="census",
-    version="0.8.11",
+    version="0.8.12",
     py_modules=['census'],
     author="Jeremy Carbaugh",
     author_email='jcarbaugh@sunlightfoundation.com',
