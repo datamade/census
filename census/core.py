e1b03d8f24df6bc5d37de1e27e8e0d635d39e337
Detinator10 <detlefk@hotmail.com>
2020-07-08 11:58:15 -0700

    Update core.py
    Added ability to query subject table

--- census/core.py
+++ census/core.py
@@ -343,7 +343,12 @@ class ACS5DpClient(ACS5Client):
 
     years = (2018, 2017, 2016, 2015, 2014, 2013, 2012)
 
+class ACS5SubjectClient(ACS5Client):
 
+    dataset = 'acs/acs5/subject'
+
+    years = (2018, 2017, 2016, 2015, 2014, 2013, 2012)
+    
 class ACS3Client(ACSClient):
 
     default_year = 2013
@@ -518,6 +523,7 @@ class Census(object):
         self.acs5 = ACS5Client(key, year, session)
         self.acs3 = ACS3Client(key, year, session)
         self.acs1 = ACS1Client(key, year, session)
+        self.acs5subject = ACS5SubjectClient(key, year, session)
         self.acs5dp = ACS5DpClient(key, year, session)
         self.acs3dp = ACS3DpClient(key, year, session)
         self.acs1dp = ACS1DpClient(key, year, session)
