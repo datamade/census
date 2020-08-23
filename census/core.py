456dca1d14d24f3c66c25041ae0f0daa13957a26
Detlef Knauss <detlefk@hotmail.com>
2020-08-23 11:52:22 -0700

    Renamed ACS5SubjectClient
    Renamed ACS5SubjectClient to ACS5StClient and acs5subject to acs5st to better match other naming conventions

--- census/core.py
+++ census/core.py
@@ -343,7 +343,7 @@ class ACS5DpClient(ACS5Client):
 
     years = (2018, 2017, 2016, 2015, 2014, 2013, 2012)
 
-class ACS5SubjectClient(ACS5Client):
+class ACS5StClient(ACS5Client):
     def _switch_endpoints(self, year):
         self.endpoint_url = 'https://api.census.gov/data/%s/acs/%s'
         self.definitions_url = 'https://api.census.gov/data/%s/acs/%s/variables.json'
@@ -527,7 +527,7 @@ class Census(object):
         self.acs5 = ACS5Client(key, year, session)
         self.acs3 = ACS3Client(key, year, session)
         self.acs1 = ACS1Client(key, year, session)
-        self.acs5subject = ACS5SubjectClient(key, year, session)
+        self.acs5st = ACS5StClient(key, year, session)
         self.acs5dp = ACS5DpClient(key, year, session)
         self.acs3dp = ACS3DpClient(key, year, session)
         self.acs1dp = ACS1DpClient(key, year, session)
