62e9d3b69559959fcac08742cf45959dfdea6ecc
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2020-08-25 11:17:23 -0500

    Bump version to v0.8.15

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.rst').read()
 
 setup(
     name="census",
-    version="0.8.14",
+    version="0.8.15",
     py_modules=['census'],
     author="Jeremy Carbaugh",
     author_email='jcarbaugh@sunlightfoundation.com',
