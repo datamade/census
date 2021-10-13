dd4f171aacc007272071a213504fb89aa3c17c80
Forest Gregg <fgregg@datamade.us>
2021-10-13 09:21:10 -0400

    update default year, closes #101

--- census/core.py
+++ census/core.py
@@ -315,7 +315,7 @@ class ACSClient(Client):
 
 class ACS5Client(ACSClient):
 
-    default_year = 2018
+    default_year = 2019
     dataset = 'acs5'
 
     years = (2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009)
@@ -347,7 +347,7 @@ class ACS5Client(ACSClient):
             geo['in'] += ' tract:{}'.format(tract)
         return self.get(fields, geo=geo, **kwargs)
 
-    @supported_years(2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011)
+    @supported_years(2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011)
     def zipcode(self, fields, zcta, **kwargs):
         warnings.warn(
             "zipcode has been deprecated; use state_zipcode instead",
@@ -358,7 +358,7 @@ class ACS5Client(ACSClient):
 
         return self.state_zipcode(fields, state_fips, zcta, **kwargs)
 
-    @supported_years(2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011)
+    @supported_years(2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011)
     def state_zipcode(self, fields, state_fips, zcta, **kwargs):
         return self.get(fields, geo={
             'for': 'zip code tabulation area:{}'.format(zcta),
@@ -404,7 +404,7 @@ class ACS3DpClient(ACS3Client):
 
 class ACS1Client(ACSClient):
 
-    default_year = 2018
+    default_year = 2019
     dataset = 'acs1'
 
     years = (2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011)
